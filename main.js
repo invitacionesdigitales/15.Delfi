296564e80b983c6a827e4d72f516f6ab8549e7c8
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2025-08-04 18:46:14 -0300

    cuatro

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function diaEventoAhora () {
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3132.091770763487!2d-57.86273212409269!3d-38.27736547186305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9585113013929f63%3A0xfde330ed42e3f500!2sCentro%20Tradicionalista%20Gauchos%20del%20Sur!5e0!3m2!1ses-419!2sar!4v1724797559488!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3403.261877243841!2d-64.36252541114504!3d-31.461981599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x942d5fe85f4c5635%3A0x3bda547de9e5d696!2sSALON%20VIP!5e0!3m2!1ses!2sar!4v1754340921642!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
         'Podes ampliar el mapa',
         
         
